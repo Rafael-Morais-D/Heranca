791f10c18512a1f3b6f5201dd13afd11565dcc07
RafaelMorais <rafaelmorais.design@gmail.com>
2023-01-12 19:19:03 -0300

    Aula de Encapsulamento e Heranã do curso de C# da Devaria

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Heranca;
+
+string nome;
+string especie;
+
+string eVenenoso;
+bool venenoso;
+
+/* Console.WriteLine("Informe o nome do mamífero:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe a espécie do mamífero:");
+especie = Console.ReadLine();
+
+Mamifero mamifero = new Mamifero(nome, especie);
+
+mamifero.Mamar(); */
+
+
+Console.WriteLine("Informe o nome do réptil:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe a espécie do réptil:");
+especie = Console.ReadLine();
+
+Console.WriteLine("Informe se o réptil é venenoso (S/N):");
+eVenenoso = Console.ReadLine();
+
+if (eVenenoso.ToLower() == "s" || eVenenoso.ToLower() == "sim")
+{
+    venenoso = true;
+} else
+{
+    venenoso = false;
+}
+
+Reptil reptil = new Reptil(nome, especie, venenoso);
+
+reptil.BotarOvo();
---
+++ Reptil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    public class Reptil : Animal
+    {
+        public bool PossuiVeneno { get; set; }
+
+        public Reptil(string nome, string especie, bool possuiVeneno)
+        {
+            Nome = nome;
+            Especie = especie;
+            PossuiVeneno = possuiVeneno;
+        }
+
+        public void BotarOvo()
+        {
+            Console.WriteLine($"Animal {Nome} da espécie {Especie} botando ovo!");
+            Console.WriteLine(PossuiVeneno ? $"{Nome} é venenoso, cuidado!": $"{Nome} não é venenoso.");
+        }
+    }
+}
